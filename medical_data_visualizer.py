3308aa70ac6813e34a9c0e54e8e048025a067bad
Mateo Chaves <locuraal32@gmail.com>
2024-04-19 22:42:37 -0500

    Actualizacion breve

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -1,9 +1,17 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sn
+import seaborn as sns
 
 #Importamos los datos
-dm = pd.read_csv("Datos.csv")
-dm.info()
-dm.head()
+df = pd.read_csv("medical_examination.csv")
+
+# Añadir la columna overweight
+df['overweight'] = np.where((df['weight'] / ((df['height']/100)**2)) > 25, 1, 0)
+print(df.head())
+
+# Normalizar gluc y cholesterol
+
+df['gluc'] = np.where((df['gluc']) > 1, 1, 0)
+df['cholesterol'] = np.where((df['cholesterol']) > 1, 1, 0)
+print(df.head(50))
