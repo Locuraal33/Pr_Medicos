8cb1cd00904daba50fa39972088cc848346be03a
Mateo Chaves <locuraal32@gmail.com>
2024-04-24 19:53:42 -0500

    Se realizo el grafico de cardio

---
+++ main.py
@@ -0,0 +1,5 @@
+import medical_data_visualizer
+from unittest import main
+
+# Test your function by calling it here
+medical_data_visualizer.draw_cat_plot()
--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -8,10 +8,36 @@ df = pd.read_csv("medical_examination.csv")
 
 # Añadir la columna overweight
 df['overweight'] = np.where((df['weight'] / ((df['height']/100)**2)) > 25, 1, 0)
-print(df.head())
 
 # Normalizar gluc y cholesterol
 
 df['gluc'] = np.where((df['gluc']) > 1, 1, 0)
 df['cholesterol'] = np.where((df['cholesterol']) > 1, 1, 0)
-print(df.head(50))
+
+# Crear el grafico 
+
+def draw_cat_plot():
+
+    # Preparación de los datos
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    
+    # Graficar con seaborn
+    fig = sns.catplot(
+        x='variable', 
+        y='total', 
+        hue='value', 
+        col='cardio',
+        kind='bar',
+        data=df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
+    )
+    
+    # Guardar la figura
+    fig.savefig('catplot.png')
+    
+    return fig
+
+df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    
+print(df_cat)
+df_cat.info()
+print(df_cat.tail(10))
